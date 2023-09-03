3a4f369cb64d33676b52c9d328601f7a027e307d
Javier Tempestelli <javiertempestelli@gmail.com>
2023-09-03 04:26:17 -0300

    Carga de algunos datos

--- ProyectoCinema/Migrations/20230903062547_DatabaseConData.cs
+++ ProyectoCinema/Migrations/20230903062547_DatabaseConData.cs
@@ -12,125 +12,18 @@ namespace ProyectoCinema.Migrations
         {
 
 
-
-
-
-            migrationBuilder.CreateTable(
-            name: "Generos",
-            columns: table => new
-     {
-             GeneroId = table.Column<int>(type: "int", nullable: false)
-                 .Annotation("SqlServer:Identity", "1, 1"),
-                              Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
-     },
-     constraints: table =>
-     {
-         table.PrimaryKey("PK_Generos", x => x.GeneroId);
-     });
-
-            migrationBuilder.CreateTable(
-                name: "Salas",
-                columns: table => new
-                {
-                    SalaId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Capacidad = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Salas", x => x.SalaId);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Peliculas",
-                columns: table => new
-                {
-                    PeliculaId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Sonopsis = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Poster = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Trailer = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    GeneroId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Peliculas", x => x.PeliculaId);
-                    table.ForeignKey(
-                        name: "FK_Peliculas_Generos_GeneroId",
-                        column: x => x.GeneroId,
-                        principalTable: "Generos",
-                        principalColumn: "GeneroId",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Funciones",
-                columns: table => new
-                {
-                    FuncionId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Horario = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    SalaId = table.Column<int>(type: "int", nullable: false),
-                    PeliculaId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Funciones", x => x.FuncionId);
-                    table.ForeignKey(
-                        name: "FK_Funciones_Peliculas_PeliculaId",
-                        column: x => x.PeliculaId,
-                        principalTable: "Peliculas",
-                        principalColumn: "PeliculaId",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Funciones_Salas_SalaId",
-                        column: x => x.SalaId,
-                        principalTable: "Salas",
-                        principalColumn: "SalaId",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Tickets",
-                columns: table => new
-                {
-                    TicketId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Usuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    FuncionId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Tickets", x => x.TicketId);
-                    table.ForeignKey(
-                        name: "FK_Tickets_Funciones_FuncionId",
-                        column: x => x.FuncionId,
-                        principalTable: "Funciones",
-                        principalColumn: "FuncionId",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Funciones_PeliculaId",
-                table: "Funciones",
-                column: "PeliculaId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Funciones_SalaId",
-                table: "Funciones",
-                column: "SalaId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Peliculas_GeneroId",
-                table: "Peliculas",
-                column: "GeneroId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Tickets_FuncionId",
-                table: "Tickets",
-                column: "FuncionId");
+     //       migrationBuilder.CreateTable(
+     //       name: "Generos",
+     //       columns: table => new
+     //{
+     //        GeneroId = table.Column<int>(type: "int", nullable: false)
+     //            .Annotation("SqlServer:Identity", "1, 1"),
+     //                         Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+     //},
+     //constraints: table =>
+     //{
+     //    table.PrimaryKey("PK_Generos", x => x.GeneroId);
+     //});
 
             migrationBuilder.InsertData(
                 table: "Generos",
